9d0bedfb836ff0ed066c37e01e3b8509e18a5896
Ricardo Perez <ricardo.perez@brooklyndata.co>
2024-04-29 16:58:33 -0600

    Update my_first_dag.py
    Add comma

--- my_first_dag.py
+++ my_first_dag.py
@@ -5,11 +5,14 @@ from airflow.operators.python import PythonOperator
 def print_a():
     print('Hi from task_a')
     
-with DAG('my_dag', 
+with DAG('my_first_dag', 
          start_date=datetime(2023, 1, 1), 
-         description='A simple tutorial DAG'
+         description='A simple tutorial DAG',
          schedule='@daily', 
          tags=['data_science'],
          catchup=False):
-    
+
     task_a = PythonOperator(task_id='task_a', python_callable=print_a)
+
+
+
