5b69e57cda23d7a44b75e1a3d90ca8a55296eed7
Ricardo Perez <ricardo.perez@brooklyndata.co>
2024-05-21 17:10:50 -0600

    Update my_dag_TaskFlow_API.py

--- my_dag_TaskFlow_API.py
+++ my_dag_TaskFlow_API.py
@@ -1,10 +1,17 @@
 from airflow.decorators import dag, task
 from datetime import datetime
 
-@dag(start_date=datetime(2023, 1, 1), description='A simple tutorial DAG', 
-     tags=['data_science'], schedule='@daily', catchup=False)
-def my_dag():
+@dag(start_date=datetime(2023, 1, 1),
+     description='A simple tutorial DAG', 
+     tags=['data_science'], 
+     schedule='@daily', 
+     catchup=False)
+def my_dag_taskflow_api():
     
     @task
     def print_a():
         print('hi from task a')
+
+    print_a()
+
+my_dag_taskflow_api()
