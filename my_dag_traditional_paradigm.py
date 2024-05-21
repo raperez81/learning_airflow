5dd0aeecbbc73ff6eb288afa5b36f3fd29cfcbd6
Ricardo Perez <ricardo.perez@brooklyndata.co>
2024-05-21 17:36:35 -0600

    Fix the parameter dag_id

--- my_dag_traditional_paradigm.py
+++ my_dag_traditional_paradigm.py
@@ -2,11 +2,16 @@ from airflow import DAG
 from datetime import datetime
 from airflow.operators.python import PythonOperator
 
-def print_a():
-    print('hi from task a')
+def print_a(name):
+    print(f'hi {name} from task a')
 
-with DAG('my_dag', start_date=datetime(2023, 1 , 1),
-         description='A simple tutorial DAG', tags=['data_science'],
-         schedule='@daily', catchup=False):
+with DAG('my_first_dag_traditional_paradigm', 
+         start_date=datetime(2023, 1 , 1),
+         description='A simple tutorial DAG', 
+         tags=['data_science'],
+         schedule='@daily', 
+         catchup=False):
     
-    task_a = PythonOperator(task_id='task_a', python_callable=print_a)
+    task_a = PythonOperator(task_id='task_a', 
+                            python_callable=print_a, 
+                            op_kwargs={'name': 'Ricardo'})
